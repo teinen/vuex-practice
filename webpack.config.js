193c765ccdb91e7bf922148427f80349a9c4e958
諦念 <teinen.github@gmail.com>
2018-06-16 17:30:31 +0900

    Add webpack-dev-server fallback setting

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,8 @@ module.exports = [
     devServer: {
       contentBase: path.resolve(__dirname, 'dist/'),
       host: '0.0.0.0',
-      port: 8080
+      port: 8080,
+      historyApiFallback: true
     },
     plugins: [
       new VueLoaderPlugin()
